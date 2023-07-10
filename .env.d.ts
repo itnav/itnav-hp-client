5cd5d428dd9cdff73d583230a154a0b40548af21
junpei-8 <miurajunpeis2@gmail.com>
2023-07-11 08:20:53 +0900

    add: 環境変数関連を整理

---
+++ .env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="astro/client" />
+
+declare global {
+  interface $InternalEnv {
+    NODE_ENV: 'development' | 'production';
+    PWD: string;
+  }
+
+  interface $ExternalEnv {
+    URL: string;
+  }
+
+  type $Env = $InternalEnv & $ExternalEnv;
+
+  var $env: $Env;
+}
+
+export {};
