4955c8b5a3c7da22334bd804924c3dcc271aad54
junpei-8 <miurajunpeis2@gmail.com>
2023-07-12 07:26:46 +0900

    update: Layout コンポーネントの基礎を調整

---
+++ src/constants/app.ts
@@ -0,0 +1,2 @@
+export const APP_ID = 'app';
+export const APP_ID_SELECTOR = `#${APP_ID}`;
---
+++ src/layouts/Layout.css.ts
@@ -0,0 +1,19 @@
+import styles, { css, style } from '@/styles';
+
+export default css.instantiate(
+  class {
+    main = style([
+      {
+        position: 'relative',
+        boxSizing: 'border-box',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        minHeight: ['100vh', '100svh'],
+        opacity: 1,
+        transition: `opacity ${styles.transitionDuration.constant.medium1} ${styles.transitionEasing.constant.standardAccelerate}`,
+      },
+    ]);
+  },
+);
