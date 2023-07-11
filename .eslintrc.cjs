19fb86f6e2a83c7ed99e3146cae48e6b823c5418
junpei-8 <miurajunpeis2@gmail.com>
2023-07-11 13:34:15 +0900

    update: Solid の導入に合わせて ESLint を更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -92,7 +92,7 @@ module.exports = {
     },
 
     {
-      files: ['**/*.{ts,cts,mts,astro}'],
+      files: ['**/*.{ts,cts,mts,tsx,astro}'],
 
       plugins: [
         /** @npm https://www.npmjs.com/package/@typescript-eslint/eslint-plugin */
@@ -131,20 +131,64 @@ module.exports = {
     },
 
     {
-      files: ['**/*.astro'],
+      files: ['**/*.{jsx,tsx}'],
 
       plugins: [
-        /** @npm https://www.npmjs.com/package/eslint-plugin-astro */
-        'astro',
+        /** @npm https://www.npmjs.com/package/eslint-plugin-react */
+        'react',
       ],
 
       extends: [
         /**
-         * @docs https://ota-meshi.github.io/eslint-plugin-astro
+         * @npm https://www.npmjs.com/package/eslint-plugin-solid
+         * @config https://github.com/solidjs-community/eslint-plugin-solid/blob/main/src/index.ts
+         */
+        'plugin:solid/typescript',
+
+        /**
+         * @npm https://www.npmjs.com/package/eslint-plugin-jsx-a11y
+         * @config https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/src/index.js
+         */
+        'plugin:jsx-a11y/strict',
+      ],
+
+      rules: {
+        /** @docs https://github.com/jsx-eslint/eslint-plugin-jsx-a11y#supported-rules */
+        'jsx-a11y/control-has-associated-label': 'error',
+
+        /** @docs https://github.com/jsx-eslint/eslint-plugin-react#list-of-supported-rules */
+        'react/jsx-sort-props': [
+          'warn',
+          {
+            noSortAlphabetically: true,
+            shorthandFirst: true,
+            shorthandLast: false,
+            callbacksLast: true,
+            ignoreCase: true,
+            multiline: 'last',
+          },
+        ],
+      },
+    },
+
+    {
+      files: ['**/*.astro'],
+
+      extends: [
+        /**
+         * @npm https://www.npmjs.com/package/eslint-plugin-astro
          * @config https://github.com/ota-meshi/eslint-plugin-astro/blob/main/src/configs/recommended.ts
          */
         'plugin:astro/recommended',
+
+        /** @config https://github.com/ota-meshi/eslint-plugin-astro/blob/main/src/a11y/configs.ts */
+        'plugin:astro/jsx-a11y-strict',
       ],
+
+      rules: {
+        /** @docs https://github.com/ota-meshi/eslint-plugin-astro#a11y-extension-rules */
+        'jsx-a11y/control-has-associated-label': 'error',
+      },
     },
 
     {
@@ -157,7 +201,7 @@ module.exports = {
     },
 
     {
-      files: ['**/components/**/*.{ts,cts,mts,astro}'],
+      files: ['**/components/**/*.{ts,cts,mts,tsx,astro}'],
 
       rules: {
         /** @docs https://typescript-eslint.io/rules */
@@ -169,7 +213,7 @@ module.exports = {
     },
 
     {
-      files: ['**/fragments/**/*.{ts,cts,mts,astro}'],
+      files: ['**/fragments/**/*.{ts,cts,mts,tsx,astro}'],
 
       rules: {
         /** @docs https://typescript-eslint.io/rules */
