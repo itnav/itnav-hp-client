6a2623b9dbf16f77e81ea64f9a17eb1302f41a6e
junpei-8 <miurajunpeis2@gmail.com>
2023-07-15 18:09:03 +0900

    update: ホームページで扱う基本的なルートを追加

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -2,9 +2,79 @@ import type { Route } from './route';
 
 export default (<const>{
   top: {
-    type: 'internal',
+    variant: 'internal',
     href: '/',
     label: 'Top',
     ariaLabel: 'Topページへ遷移する',
+  },
+
+  about: {
+    variant: 'internal',
+    href: '/about',
+    label: 'About',
+    ariaLabel: 'Aboutページへ遷移する',
+  },
+
+  services: {
+    variant: 'internal',
+    href: '/services',
+    label: 'Services',
+    ariaLabel: 'Servicesページへ遷移する',
+  },
+
+  members: {
+    variant: 'internal',
+    href: '/members',
+    label: 'Members',
+    ariaLabel: 'Membersページへ遷移する',
+  },
+
+  blogs: {
+    variant: 'internal',
+    href: '/blogs',
+    label: 'News&Reports',
+    ariaLabel: 'News&Reportsページへ遷移する',
+  },
+
+  blog: {
+    variant: 'internal',
+    getHref: (id: string) => `/blogs/${id}`,
+    label: 'News&Report',
+    ariaLabel: 'News&Reportページへ遷移する',
+  },
+
+  contact: {
+    variant: 'external',
+    href: 'https://forms.gle/fB1XMDEJjVGGBGX76',
+    label: 'Contact',
+    ariaLabel: 'Contactページへ遷移する',
+  },
+
+  privacy: {
+    variant: 'internal',
+    href: '/privacy',
+    label: 'Privacy Policy',
+    ariaLabel: 'Privacyページへ遷移する',
+  },
+
+  terms: {
+    variant: 'internal',
+    href: '/terms',
+    label: 'Terms',
+    ariaLabel: 'Termsページへ遷移する',
+  },
+
+  recruit: {
+    variant: 'external',
+    href: 'https://recruit.itnav.co.jp',
+    label: 'Recruit',
+    ariaLabel: 'Recruitページへ遷移する',
+  },
+
+  recruitInfo: {
+    variant: 'external',
+    href: 'https://recruit.itnav.co.jp/information',
+    label: 'Recruit Info',
+    ariaLabel: 'Recruitページへ遷移する',
   },
 }) satisfies Record<string, Route>;
